ffe65c76505ba6de3cf9215471806abd86e31dbf
wenwen0318 <wen0318asd@gmail.com>
2022-12-01 17:08:22 +0800

    Get Camera Permission MainActivity

--- app/src/main/java/com/example/posebymlkit/MainActivity.java
+++ app/src/main/java/com/example/posebymlkit/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.posebymlkit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvRes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +25,33 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView bottomNav = findViewById(R.id.navigation);
         bottomNav.setOnItemSelectedListener(navListener);
         getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new PoseListFragment()).commit();
+
+        tvRes = findViewById(R.id.textView_PermissionResult);
+
+        boolean cameraHasGone = checkSelfPermission(Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED;
+
+        boolean externalHasGone = checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            String[] permissions;
+            if (!cameraHasGone && !externalHasGone) {//如果兩個權限都未取得
+                permissions = new String[2];
+                permissions[0] = Manifest.permission.CAMERA;
+                permissions[1] = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+            } else if (!cameraHasGone) {//如果只有相機權限未取得
+                permissions = new String[1];
+                permissions[0] = Manifest.permission.CAMERA;
+            } else if (!externalHasGone) {//如果只有存取權限未取得
+                permissions = new String[1];
+                permissions[0] = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+            } else {
+                tvRes.setText("相機權限已取得\n儲存權限已取得");
+                return;
+            }
+            requestPermissions(permissions, 100);
+        }
     }
 
     private final BottomNavigationView.OnItemSelectedListener navListener = item -> {
@@ -39,4 +73,32 @@ public class MainActivity extends AppCompatActivity {
         }
         return true;
     };
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        StringBuffer word = new StringBuffer();
+        switch (permissions.length) {
+            case 1:
+                if (permissions[0].equals(Manifest.permission.CAMERA)) word.append("相機權限");
+                else word.append("儲存權限");
+                if (grantResults[0] == 0) word.append("已取得");
+                else word.append("未取得");
+                word.append("\n");
+                if (permissions[0].equals(Manifest.permission.CAMERA)) word.append("儲存權限");
+                else word.append("相機權限");
+                word.append("已取得");
+
+                break;
+            case 2:
+                for (int i = 0; i < permissions.length; i++) {
+                    if (permissions[i].equals(Manifest.permission.CAMERA)) word.append("相機權限");
+                    else word.append("儲存權限");
+                    if (grantResults[i] == 0) word.append("已取得");
+                    else word.append("未取得");
+                    if (i < permissions.length - 1) word.append("\n");
+                }
+                break;
+        }
+        tvRes.setText(word.toString());
+    }
 }
